e0c0d60126aafeb69e3685f0fd7c1e53d8cc2504
CherrylipsW <1027283324@qq.com>
2017-06-15 23:31:14 +0800

    Add files via upload

---
+++ js/func.js
@@ -0,0 +1,291 @@
+function getBgColor(number){
+	var color = "#000000"
+	switch(number){
+		case 2: color="#EEE4DA";
+				break;
+		case 4: color="#EDE0C8";
+				break;
+		case 8: color="#F2B179";
+				break;
+		case 16:color="#F59563";
+				break;
+		case 32: color="#F67F5F";
+				 break;
+		case 64: color="#F65E3A";
+				 break;
+		case 128: color="#EDCF72";
+				 break;
+		case 256: color="#EDCC61";
+				  break;
+		case 512: color="#EDC850";
+				  break;
+		case 1024: color="#EDC53F";
+				   break;
+		case 2048: color="#EDC212";
+				   break;
+	}
+
+	return color;
+}
+
+function getColor(number){
+	var color="#000000";
+	if(number<=4){
+		color = "#776E65";
+	}else{
+		color = "#F9F6F2";
+	}
+
+	return color;
+}
+
+function getANumber(speed,board){
+	if(noSpace(board)){
+		return false;
+	}
+	var ranX = parseInt(Math.floor(Math.random()*4));
+	var ranY = parseInt(Math.floor(Math.random()*4));
+
+	var time = 0;//寻找空格的次数
+	while(time<=50){
+		if(!board[ranX][ranY]){
+			break;
+		}
+
+		var ranX = parseInt(Math.floor(Math.random()*4));
+		var ranY = parseInt(Math.floor(Math.random()*4));
+
+		time++;
+	}
+
+	if(time>50){
+		for(var i=0;i<4;i++){
+			for (var j=0;j<4;j++){
+				if(board[i][j]==0){
+					ranX = i;
+					ranY = j;
+				}
+			}
+		}
+	}
+	//随机生成数字2或4，概率分别为2/3,1/3
+	var num = Math.random()<0.67? 2:4;
+	showNumber(ranX,ranY,num,speed);
+	board[ranX][ranY] = num;
+
+}
+
+function noSpace(board){
+	for (var i=0;i<4;i++) {
+		for(var j=0;j<4;j++){
+			if(board[i][j]==0)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+function showNumber(i,j,num,speed){
+	var row = $(".row-grid-container");
+	var col = row.eq(i).children(".number-cell");
+
+	col.eq(j).css({
+		"background-color":getBgColor(num),
+		"color":getColor(num)
+	});
+
+	col.eq(j).animate({
+		"width":$(".grid-cell").width(),
+		"height":$(".grid-cell").height(),
+		"top":getTop(i,j),
+		"left":getLeft(i,j)
+	},speed);
+
+	if(num.toString().length>=4){
+		col.eq(j).css({
+			"font-size":parseInt($(".grid-cell").css("font-size"))-5+"px"
+		});
+	}
+
+	col.eq(j).text(num);
+}
+
+
+
+function getTop(i,j){
+	var row = $(".row-grid-container");
+	var col = row.eq(i).children(".grid-cell");
+	var colTop = col.eq(j).offset().top;
+	var containerTop = $(".game-container").offset().top;
+
+	var top = colTop - containerTop;
+
+	return top;
+}
+
+function getLeft(i,j){
+	var row = $(".row-grid-container");
+	var col = row.eq(i).children(".grid-cell");
+	var colLeft = col.eq(j).offset().left;
+	var containerLeft = $(".game-container").offset().left;
+
+	var left = colLeft - containerLeft;
+
+	return left;
+}
+
+function canMoveLeft(board){
+	//只需遍历除最左边一列的剩余三列的格子
+	for(var i=0;i<4;i++){
+		for(var j=1;j<4;j++){
+			if(board[i][j]!=0){
+				if(board[i][j-1]==0 || board[i][j-1]==board[i][j])
+					return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function canMoveUp(board){
+	//只需遍历除最上一行的剩余三行的格子
+	for(var i=1;i<4;i++){
+		for(var j=0;j<4;j++){
+			if(board[i][j]!=0){
+				if(board[i-1][j]==0 || board[i-1][j]==board[i][j])
+					return true;
+			}
+		}
+	}
+
+	return false;
+}
+function canMoveRight(board){
+	for(var i=0;i<4;i++){
+		for(var j=0;j<3;j++){
+			if(board[i][j]!=0){
+				if(board[i][j+1]==0 || board[i][j+1] == board[i][j])
+					return true;
+			}
+		}
+	}
+
+	return false;
+}
+function canMoveDown(board){
+	for(var i=0;i<3;i++){
+		for(var j=0;j<4;j++){
+			if(board[i][j]!=0){
+				if(board[i+1][j] == 0 || board[i+1][j] == board[i][j])
+					return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function isGameOver(board){
+	if(isWin(board)){
+		gameover("You Win!");
+	}
+
+	if(noSpace(board) && !canMoveLeft(board) && !canMoveUp(board) && !canMoveRight(board) && !canMoveDown(board)){
+		gameover("Game Over!");
+	}
+}
+
+function ifGameOver(board){
+	if(isWin(board) || (noSpace(board) && !canMoveLeft(board) && !canMoveUp(board) && !canMoveRight(board) && !canMoveDown(board))){
+		return true;
+	}
+	return false;
+}
+
+function gameover(string){
+	$(".game-over").css({
+		"line-height":$(".game-container").height() + "px"
+	});
+
+	$(".game-over").animate({
+		"top":0
+	});
+
+	$(".game-over").text(string);
+}
+
+function isWin(board){
+	for(var i=0;i<4;i++){
+		for(var j=0;j<4;j++){
+			if(board[i][j] == 2048){
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+
+function noObstacle(row1,col1,row2,col2,board){
+
+	if(row1 == row2){
+		for(var i=col1+1;i<col2;i++){
+			if(board[row1][i]!=0)
+				return false;
+		}
+	}
+
+	if(col1 == col2){
+		for(var i=row1+1;i<row2;i++){
+			if(board[i][col1] != 0)
+				return false;
+		}
+	}
+
+	return true;
+}
+
+function move(xFrom,yFrom,xTo,yTo){
+	var col = $(".row-grid-container").eq(xFrom).children(".number-cell");
+	col.eq(yFrom).animate({
+		"top":getTop(xTo,yTo),
+		"left":getLeft(xTo,yTo)
+	},200);
+
+}
+
+function strToDbarr(string){
+	var arr1 = string.split(",");
+	for(var i=3;i<arr1.length-4;i+=4){
+		arr1[i] += " ";
+	}
+	string = arr1.join(".");
+	arr1 = string.split(" .");
+
+	for(var j=0;j<arr1.length;j++){
+		arr1[j] = arr1[j].split(".");
+	}
+
+	for(var i=0;i<arr1.length;i++){
+		for(j=0;j<arr1[i].length;j++){
+			arr1[i][j] = parseInt(arr1[i][j]);
+		}
+	}
+
+	return arr1;
+}
+
+function isNew(board){
+	for(var i=0;i<board.length;i++){
+		for(var j=0;j<board[i].length;j++){
+			if(board[i][j]!=0)
+				return false;
+		}
+	}
+
+	return true;
+}
---
+++ js/init.js
@@ -0,0 +1,279 @@
+var board = [];//储存数字方格变化的数字
+var score = 0;
+var bestScore = 0;
+
+(function(){
+	score = parseInt(localStorage.score);
+	bestScore = parseInt(localStorage.bestScore);
+
+	debugger
+	newGame(isNew(strToDbarr(localStorage.board)) || ifGameOver(strToDbarr(localStorage.board)));
+})();
+
+function newGame(newG){
+
+	
+
+	//刚开始要随机生成两个数字才能开始游戏
+	if(newG){
+		init();
+		getANumber(200,board);
+		getANumber(200,board);
+		storage();
+	}else{
+		board = strToDbarr(localStorage.board);
+		updateBoard();
+	}
+
+	$(".game-over").css({"top":$(".game-container").height()});
+
+	$("#current-score").text(score);
+ 	
+ 	$("#best-score").text(bestScore);
+
+}
+
+function init(){
+	//初始化board
+	var row = $(".row-grid-container");
+	for(var i=0;i<row.length;i++){
+		var col = row.eq(i).children(".grid-cell");
+		board[i] = [];
+		for(var j=0;j<col.length;j++){
+			board[i][j] = 0;
+		}
+	}
+
+
+	//当有操作时，更新board数组数据，并显示
+	updateBoard();
+
+}
+
+function updateBoard(){
+	$(".number-cell").remove();
+
+	var row = $(".row-grid-container");
+	for(var i=0;i<row.length;i++){
+		var col = row.eq(i).children(".grid-cell");
+		for(var j=0;j<col.length;j++){
+			row.eq(i).append("<div class='number-cell'></div>");
+			var numberCell = row.eq(i).children(".number-cell");
+			if(board[i][j]==0){
+				numberCell.eq(j).css({
+					"width":0,
+					"height":0,
+					"top":getTop(i,j)+$(".grid-cell").width()/2,//将没有数字的空格的位置调整到格子中间，为了动画显示好看
+					"left":getLeft(i,j)+$(".grid-cell").height()/2
+				});
+			}else{
+				numberCell.eq(j).css({
+					"width":$(".grid-cell").width(),
+					"height":$(".grid-cell").height(),
+					"top":getTop(i,j),
+					"left":getLeft(i,j),
+					"background-color":getBgColor(board[i][j]),
+					"color":getColor(board[i][j])
+				});
+
+				if(board[i][j].toString().length>=4){
+					numberCell.eq(j).css({
+						"font-size":parseInt($(".grid-cell").css("font-size"))-5+"px"
+					});
+				}
+
+				numberCell.eq(j).text(board[i][j]);
+				//showNumber(i,j,board[i][j],1);
+			}
+		}
+	}
+
+	$("#current-score").text(score);
+
+	if(score >= bestScore){
+		$("#best-score").text(score);
+		bestScore = score;
+	};
+
+	storage();
+}
+
+$(".new-game").on("click",function(){
+	score = 0;
+	newGame(true);
+});
+
+$(document).keydown(function(ev) {
+	switch(ev.keyCode){
+		case 37:
+				if(canMoveLeft(board)){
+					moveLeft();
+					getANumber(1000,board);
+					storage();
+					isGameOver(board);
+				};
+				break;
+		case 38:if(canMoveUp(board)){
+					moveUp();
+					getANumber(1000,board);
+					storage();
+					isGameOver(board);
+				};
+				break;
+		case 39:
+				if(canMoveRight(board)){
+					moveRight();
+					getANumber(1000,board);
+					storage();
+					isGameOver(board);
+				};
+				break;
+		case 40:
+				if(canMoveDown(board)){
+					moveDown();
+					getANumber(1000,board);
+					storage();
+					isGameOver(board);
+				};
+				break;
+	}
+});
+
+
+function moveLeft(){
+	//避免按一下键造成整排或者整列的一次性添加
+	var noRepeatAdd = [[true,true,true,true],[true,true,true,true],[true,true,true,true],[true,true,true,true]];
+
+	for(var i=0;i<4;i++){
+		for(var j=1;j<4;j++){
+
+			if(board[i][j]!=0){
+				for(var k=0;k<j;k++){
+
+					if(board[i][k]==0 && noObstacle(i,k,i,j,board)){
+						move(i,j,i,k);
+						board[i][k] = board[i][j];
+						board[i][j] = 0;
+						break;
+					}else if(board[i][k] == board[i][j] && noObstacle(i,k,i,j,board) && noRepeatAdd[i][k]){
+						move(i,j,i,k);
+						board[i][k] += board[i][j];
+						board[i][j] = 0;
+						noRepeatAdd[i][k] = false;
+						score += board[i][k];
+						break;
+					}
+				}
+			}
+		}
+	}
+	setTimeout("updateBoard()",220);
+}
+
+function moveUp(){
+	//避免按一下键造成整排或者整列的一次性添加
+	var noRepeatAdd = [[true,true,true,true],[true,true,true,true],[true,true,true,true],[true,true,true,true]];
+
+	for(var i=1;i<4;i++){
+		for(var j=0;j<4;j++){
+
+			if(board[i][j]!=0){
+				for(var k=0;k<i;k++){
+
+					if(board[k][j] == 0 && noObstacle(k,j,i,j,board)){
+						move(i,j,k,j);
+						board[k][j] = board[i][j];
+						board[i][j] = 0;
+						break;
+					}else if(board[k][j] == board[i][j] && noObstacle(k,j,i,j,board) && noRepeatAdd[k][j]){
+						move(i,j,k,j);
+						board[k][j] += board[i][j];
+						board[i][j] = 0;
+						noRepeatAdd[k][j] = false;
+						score += board[k][j];
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	setTimeout("updateBoard()",220);
+}
+
+function moveRight(){
+	//避免按一下键造成整排或者整列的一次性添加
+	var noRepeatAdd = [[true,true,true,true],[true,true,true,true],[true,true,true,true],[true,true,true,true]];
+
+	for(var i=0;i<4;i++){
+		for(var j=2;j>=0;j--){
+
+			if(board[i][j] != 0){
+				for(var k=3;k>j;k--){
+					if(board[i][k]==0 && noObstacle(i,j,i,k,board)){
+						move(i,j,i,k);
+						board[i][k] = board[i][j];
+						board[i][j] = 0;
+						break;
+					}else if(board[i][k] == board[i][j] && noObstacle(i,j,i,k,board) && noRepeatAdd[i][k]){
+						move(i,j,i,k);
+						board[i][k] += board[i][j];
+						board[i][j] = 0;
+						noRepeatAdd[i][k] = false;
+						score += board[i][k];
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	setTimeout("updateBoard()",220);
+}
+
+function moveDown(){
+	//避免按一下键造成整排或者整列的一次性添加
+	var noRepeatAdd = [[true,true,true,true],[true,true,true,true],[true,true,true,true],[true,true,true,true]];
+
+	for(var i=2;i>=0;i--){
+		for(var j=0;j<4;j++){
+
+			if(board[i][j] != 0){
+				for(var k=3;k>i;k--){
+
+					if(board[k][j] == 0 && noObstacle(i,j,k,j,board)){
+						move(i,j,k,j);
+						board[k][j] = board[i][j];
+						board[i][j] = 0;
+						break;
+					}else if(board[k][j] == board[i][j] && noObstacle(i,j,k,j,board) && noRepeatAdd[k][j]){
+						move(i,j,k,j);
+						board[k][j] += board[i][j];
+						board[i][j] = 0;
+						noRepeatAdd[k][j] = false;
+						score += board[k][j];
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	setTimeout("updateBoard()",220);
+}
+
+function storage(){
+	if(window.localStorage){
+		var storage = window.localStorage;
+
+		storage.score = $("#current-score").text();
+		storage.bestScore = $("#best-score").text();
+		storage.board = board;
+
+		console.log(storage.score + " " + storage.bestScore);
+			
+	}else{
+		alert("Your broswer doesn't support localstorage!");
+	}
+}
+
